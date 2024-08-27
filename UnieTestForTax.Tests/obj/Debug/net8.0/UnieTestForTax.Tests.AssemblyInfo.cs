1491cee8bcc8ad209551ed1030a63a79d0641900
David Chen <162773720+33678@users.noreply.github.com>
2024-08-27 22:45:26 +0800

    commit

--- UnieTestForTax.Tests/obj/Debug/net8.0/UnieTestForTax.Tests.AssemblyInfo.cs
+++ UnieTestForTax.Tests/obj/Debug/net8.0/UnieTestForTax.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnieTestForTax.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e9e14883b7cd00224edc79953ffb5d7a61ed69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895e2381928a23fc93f8e7e0010d59e4cfa9c4a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnieTestForTax.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnieTestForTax.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
